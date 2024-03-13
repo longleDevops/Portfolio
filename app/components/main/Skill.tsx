f8ba488b6da203f8076e49eb569f575960504fbd
long <137044122+longleDevops@users.noreply.github.com>
2024-03-13 03:32:01 -0700

    fix: fix missing key props

--- app/components/main/Skill.tsx
+++ app/components/main/Skill.tsx
@@ -2,7 +2,7 @@ export const SkillList = ({ skills }: { skills: string[] }) => {
   return (
     <div className="flex flex-wrap gap-2 pb-2 ">
       {skills.map((skill) => (
-        <span className="p-2 text-[10px] text-white bg-black rounded-lg">
+        <span className="p-2 text-[10px] text-white bg-black rounded-lg" key={skill}>
           {skill}
         </span>
       ))}
